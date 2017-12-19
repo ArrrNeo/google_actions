94b17ba90fef58397fbf16140c2d2734f0be9fa2
Naveen Rawat <rawat.nav@gmail.com>
2017-12-19 07:45:32 -0800

    update firebase app to make http request

--- firebase_app/functions/index.js
+++ firebase_app/functions/index.js
@@ -1,15 +1,17 @@
 "use strict";
 const functions = require('firebase-functions');
 var ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+// to comunicate with raspberry pi
+var request = require('request');
 
 exports.yourFunction = functions.https.onRequest((req, res) => {
-	let name;
-	let app = new ActionsSdkApp({request: req, response: res});
-
-	function handleMainIntent(app) {
-		app.tell('ending conversation!');
-	}
-	app.handleRequest(handleMainIntent);
-    res.status(200);
-    res.end();
+	request('http://67.180.85.58:8080/get_room_temp', function (error, response, body) {
+		let app = new ActionsSdkApp({request: this.my_req, response: this.my_res});
+		//console.log('error:', error);
+		//console.log('statusCode:', response && response.statusCode);
+		//console.log('body:', body);
+		app.tell(body);
+		this.my_res.status(200);
+		this.my_res.end();
+	}.bind({my_req: req, my_res: res}));
 });
